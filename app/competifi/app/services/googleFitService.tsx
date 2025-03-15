38d77d83bc0dbf137dceba588e8489e634280d15
alokj10 <alok_j10@yahoo.co.in>
2025-03-16 00:47:01 +0530

    google fit integration

--- app/competifi/app/services/googleFitService.tsx
+++ app/competifi/app/services/googleFitService.tsx
@@ -1,6 +1,7 @@
-import GoogleFit, { Scopes } from 'react-native-google-fit';
+import GoogleFit, { BucketUnit, Scopes } from 'react-native-google-fit';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { requestPermission, initialize, readRecord, readRecords } from 'react-native-health-connect';
 
 const authorizeGoogleFitDataFetch = async() => {
   // await GoogleSignin.hasPlayServices();
@@ -9,9 +10,16 @@ const authorizeGoogleFitDataFetch = async() => {
   
 
     const options = {
-        scopes: [Scopes.FITNESS_ACTIVITY_READ], // Only read step count
+        scopes: [
+          Scopes.FITNESS_ACTIVITY_READ,
+          Scopes.FITNESS_ACTIVITY_WRITE
+        ], // Only read step count
       };
       try {
+        if(GoogleFit.isAuthorized) {
+          console.log('GoogleFit is already authorized, re-authorizing...');
+          GoogleFit.disconnect();
+        }
         const authResult = await GoogleFit.authorize(options)
         console.log('authResult', authResult);
         if (authResult.success) {
@@ -31,19 +39,71 @@ interface CompetifiGoogleFitModel {
   steps: number;
 }
 
-const getGoogleFitData = async (startDate: string, endDate: string): Promise<CompetifiGoogleFitModel[]> => {
-    await authorizeGoogleFitDataFetch();
+const requestPermissions = () => {
+  requestPermission([
+    {
+      accessType: 'read',
+      recordType: 'Steps',
+    }
+  ]).then((permissions) => {
+    console.log('Granted permissions ', { permissions });
+  });
+};
+const readSampleDataSingle = () => {
+  initialize();
+  requestPermissions();
 
+  // readRecord(
+  //   'Steps',
+  //   'a7bdea65-86ce-4eb2-a9ef-a87e6a7d9df2'
+  // ).then((result) => {
+  //   console.log('Retrieved record: ', JSON.stringify({ result }, null, 2));
+  // });
+};
+
+const getGoogleFitData = async (startDate: string, endDate: string): Promise<CompetifiGoogleFitModel[]> => {
+    // readSampleDataSingle();
+    // console.log('Reading records: ');
+    // const { records } = await readRecords('Steps', {
+    //   timeRangeFilter: {
+    //     operator: 'between',
+    //     startTime: '2024-11-09T12:00:00.405Z',
+    //     endTime: '2025-03-14T23:53:15.405Z',
+    //   },
+    // });
+    // console.log('records', records);
+    // let steps = await readRecord('Steps', 'a7bdea65-86ce-4eb2-a9ef-a87e6a7d9df2');
+    // console.log('steps', steps);
+    // readSampleDataSingle();
+
+        // Transform the data into CompetifiGoogleFitModel format
+        // const formattedData: CompetifiGoogleFitModel[] = [];
+        // return formattedData;
+
+
+    await authorizeGoogleFitDataFetch();
     const options = {
         startDate: startDate, // Start date in ISO8601 format
         endDate: endDate || new Date().toISOString(), // End date as provided or current time
     };
-
-    console.log('options', options);
+    const opt = {
+      startDate: "2025-03-01T00:00:17.971Z", // required ISO8601Timestamp
+      endDate: endDate || new Date().toISOString(), // required ISO8601Timestamp
+      bucketUnit: BucketUnit.MINUTE, // optional - default "DAY". Valid values: "NANOSECOND" | "MICROSECOND" | "MILLISECOND" | "SECOND" | "MINUTE" | "HOUR" | "DAY"
+      bucketInterval: 15, // optional - default 1. 
+    };
+    console.log('options', opt);
   
     try {
-        const res = await GoogleFit.getDailyStepCountSamples(options);
-        const stepsData = res.find(data => data.source === 'com.google.android.gms');
+        const res = await GoogleFit.getDailyStepCountSamples(opt);
+        // console.log('daily steps count samples', res);
+        // const weights = await GoogleFit.getWeightSamples(opt);
+        // console.log('daily weight', weights);
+        const dailySteps = await GoogleFit.getDailySteps();
+        // const r1 = await GoogleFit.startRecording(options);
+        console.log('daily steps', dailySteps);
+        // const stepsData = dailySteps.find(data => data.source === 'com.google.android.gms:estimated_steps');
+        const stepsData = res.find(data => data.source === 'com.google.android.gms:estimated_steps');
       
         if (!stepsData || !stepsData.steps) {
           console.log('No steps data found');
